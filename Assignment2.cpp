ce2e5dd54dd6cefc6b2ace311025e7f8b10fd318
Divya Prakash Jha <prakashdivya0607@gmail.com>
2023-01-08 11:00:09 +0530

    Updated Assignment-2

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -3,97 +3,68 @@
 using namespace std;
 #define nl "\n"
 
-map<char, int>m = {{'(', 6}, {'^', 5}, {'*', 4}, {'/', 3}, {'+', 2}, {'-', 1}};
+map<char, int>precedence = { {'^', 5}, {'*', 4}, {'/', 3}, {'+', 2}, {'-', 1} };
 
+
+template<class T>
 class Node{
     public:
-        char data;
-        int data1;
+        T data;
         Node *next;
+
+        Node(T x){
+            data = x;
+            next = nullptr;
+        }
 };
 
+template<class T>
 class Stack{ // Stack as an ADT
-    Node *top;
+    Node<T> *top;
 
     public:
         Stack(){
-            top = NULL;
+            top = nullptr;
         }
 
-        void push(char d){ // for characters
-            Node *temp = new Node();
-            temp->data = d;
+        void push(T d){
+            Node<T> *temp = new Node<T>(d);
             
             temp->next = top;
             top = temp;
         }
 
-        void push1(int d){ // for integers
-            Node *temp = new Node();
-            temp->data1 = d;
-            
-            temp->next = top;
-            top = temp;
-        }
-
-        char pop(){ // for characters
-            if (top == NULL){
-                return ' ';
+        void pop(){
+            if (top == nullptr){
+                return;
             }
             else{ 
-                Node *temp = top;
+                Node<T> *temp = top;
                 top = top->next;
 
-                char el = temp->data;
                 free(temp);
-                return el;
             }
         }
 
-        int pop1(){ // for integers
-            if (top == NULL){
-                return 1;
-            }
-            else{ 
-                Node *temp = top;
-                top = top->next;
-
-                int el = temp->data1;
-                free(temp);
-                return el;
-            }
-        }
-
-        char tp(){ // for characters
+        T tp(){
             return top->data;
         }
 
-        int tp1(){ // for integers
-            return top->data1;
-        }
-
         bool empty(){
-            return top == NULL ? true : false;
+            return (top == nullptr) ? true : false;
         }
 
-        void display(){ // for characters
-            Node *temp = top;
-            while(temp != NULL){
+        void display(){
+            Node<T> *temp = top;
+            while(temp != nullptr){
                 cout<<temp->data<<nl;
-                temp = temp->next;
-            }
-        }
-
-        void display1(){ // for integers
-            Node *temp = top;
-            while(temp != NULL){
-                cout<<temp->data1<<nl;
                 temp = temp->next;
             }
         }    
 };
 
-string infixToPostfix(Stack &st, string infix){
+string infixToPostfix(string infix){
+    Stack<char> st;
     string postfix = "";
 
     for(auto it : infix){
@@ -109,22 +80,24 @@ string infixToPostfix(Stack &st, string infix){
         else if (st.empty()){
             st.push(it);
         }
+        else if (st.tp() == '('){
+            st.push(it);
+        }
         else if (it == ')'){
                 while(st.tp() != '('){
-                    postfix += st.pop();
+                    postfix += st.tp();
+                    st.pop();
                 }
                 st.pop();
         }
         else{
-            if(st.tp() == '('){
-                st.push(it);
-            }
-            else if (m[it] >= m[st.tp()]){
+            if (precedence[it] >= precedence[st.tp()]){
                 st.push(it);    
             }
-            else if (m[it] < m[st.tp()]){
-                while(!st.empty() and m[it] < m[st.tp()] and st.tp() != '('){
-                    postfix += st.pop();
+            else if (precedence[it] < precedence[st.tp()]){
+                while(!st.empty() and st.tp() != '(' and precedence[it] < precedence[st.tp()] ){
+                    postfix += st.tp();
+                    st.pop();
                 }
                 st.push(it);
             }            
@@ -134,7 +107,8 @@ string infixToPostfix(Stack &st, string infix){
 
     while(!st.empty()){
         if(st.tp() != '('){
-            postfix += st.pop();
+            postfix += st.tp();
+            st.pop();
         }
         else{
             return "Invalid Expression";
@@ -144,9 +118,10 @@ string infixToPostfix(Stack &st, string infix){
     return postfix;
 }
 
-string infixToPrefix(Stack &st, string infix){
+string infixToPrefix(string infix){
     string prefix = "";
     reverse(infix.begin(), infix.end());
+
     for(auto &it : infix){
         if (it == ')'){
             it = '(';
@@ -156,81 +131,89 @@ string infixToPrefix(Stack &st, string infix){
         }
     }
 
-    prefix = infixToPostfix(st, infix);
+    prefix = infixToPostfix(infix);
     reverse(prefix.begin(), prefix.end());
 
     return prefix;
 }
 
-int postfixEvaluation(Stack &st, string postfix, bool flag){
+int postfixEvaluation(string postfix, bool flag){ // flag = 1 (Used for postfix evaluation), flag = 0 (Used for prefix evaluation)
+    Stack<int>st;
     
     for(auto it : postfix){
         if ((it >= 'a' and it <= 'z') or (it >= 'A' and it <= 'Z')){
             int temp;
             cout<<"Enter the value of "<<it<<" :";
             cin>>temp;
-            st.push1(temp);
+            st.push(temp);
         }
         else if (it >= '0' and it <= '9'){
-            st.push1(it - '0');
+            st.push(it - '0');
         }
         else{
             int a = 0, b = 0;
 
             if (flag){
-                a = st.pop1();
-                b = st.pop1();
+                a = st.tp(); st.pop();
+                b = st.tp(); st.pop();
             }
             else{
-                b = st.pop1();
-                a = st.pop1();
+                b = st.tp(); st.pop();
+                a = st.tp(); st.pop();
             }
             
 
             if (it == '^'){
-                st.push1(pow(b, a));
+                st.push(pow(b, a));
             }
             else if (it == '/'){
                 if(a == 0){
                     cout<<"Cannot divide by zero";
                     return 0;
                 }
-                st.push1(b/a);
+                st.push(b/a);
             }
             else if (it == '*'){
-                st.push1(b*a);
+                st.push(b*a);
             }
             else if(it == '+'){
-                st.push1(b+a);
+                st.push(b+a);
             }
             else if(it == '-'){
-                st.push1(b-a);
+                st.push(b-a);
             }
         }
     }
-    return st.tp1();
+    
+    int ev = st.tp();
+
+    st.pop();
+    if(!st.empty()){
+        cout<<"Invalid Expression ";
+    }
+
+    return ev;
 }
 
-int prefixEvaluation(Stack &st, string prefix){
+int prefixEvaluation(string prefix){
     reverse(prefix.begin(), prefix.end());
 
-    return postfixEvaluation(st, prefix, 0);
+    return postfixEvaluation(prefix, 0);
 }
 
 int main(){
-    Stack st;
 
     int op = 0;
     string infix, postfix, prefix;
 
     do{
-        cout<<"\n*** Operations ***\n"
+        cout<<"\n\n********** Operations **********\n"
             <<"    1. Infix to postfix\n"
             <<"    2. Infix to prefix\n"
             <<"    3. Postfix Evaluation\n"
             <<"    4. Prefix Evaluation\n"
-            <<"    0. Exit\n";
-
+            <<"    0. Exit\n"
+            <<"**********************************\n";
         cin>>op;
 
         switch(op){
@@ -238,34 +221,26 @@ int main(){
                 cout<<"Enter the Infix Expression : ";
                 cin>>infix;
                 
-                cout<<"Postfix Expression : "<<infixToPostfix(st, infix);
+                cout<<"Postfix Expression : "<<infixToPostfix(infix);
                 break;
 
             case 2:
                 cout<<"Enter the Infix Expression : ";
                 cin>>infix;
 
-                cout<<"Prefix Expression : "<<infixToPrefix(st, infix);
+                cout<<"Prefix Expression : "<<infixToPrefix(infix);
                 break;
 
             case 3:
                 cout<<"Enter the Postfix Expression : ";
                 cin>>postfix;
-                cout<<postfixEvaluation(st, postfix, 1);
+                cout<<postfixEvaluation(postfix, 1);
                 break;
             case 4:
                 cout<<"Enter the Prefix Expression : ";
                 cin>>prefix;
-                cout<<prefixEvaluation(st, prefix);
+                cout<<prefixEvaluation(prefix);
                 break;
-        }
-
-        if(op == 3 or op == 4){
-            st.pop1();
-            while(!st.empty()){
-                cout<<"Invalid Expression ";
-                st.pop1();
-            }
         }
 
     }while(op!=0);
